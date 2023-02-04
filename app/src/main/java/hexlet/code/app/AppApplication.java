5c631919cdd11a0d1d5523afaf45435f1ca736db
redgradient <neutral1shade@yahoo.com>
2023-02-04 18:22:01 +0400

    add class to git

---
+++ app/src/main/java/hexlet/code/app/AppApplication.java
@@ -0,0 +1,18 @@
+package hexlet.code.app;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        // SpringApplication.run(AppApplication.class, args);
+        SpringApplication application = new SpringApplication(AppApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
+
+    }
+
+}
