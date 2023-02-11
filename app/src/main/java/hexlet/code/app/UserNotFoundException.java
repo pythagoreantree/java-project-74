912cf753800a7920e8fe11fb108e0ab3762ead61
redgradient <neutral1shade@yahoo.com>
2023-02-12 01:45:24 +0400

    add UserNotFoundException.java

---
+++ app/src/main/java/hexlet/code/app/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package hexlet.code.app;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(long id) {
+        super("User " + id + " not found");
+    }
+}
