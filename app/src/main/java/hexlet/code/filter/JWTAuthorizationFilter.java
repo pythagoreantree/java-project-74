f691fc4fdcbf5a5095fa0f2adb7f33c8dfd10145
redgradient <neutral1shade@yahoo.com>
2023-02-20 01:05:42 +0400

    commit

--- app/src/main/java/hexlet/code/filter/JWTAuthorizationFilter.java
+++ app/src/main/java/hexlet/code/filter/JWTAuthorizationFilter.java
@@ -20,7 +20,6 @@ import static org.springframework.security.web.authentication.UsernamePasswordAu
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
     private static final String BEARER = "Bearer";
-
     private final RequestMatcher publicUrls;
     private final JWTHelper jwtHelper;
 
