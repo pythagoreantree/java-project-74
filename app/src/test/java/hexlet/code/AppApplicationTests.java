9a65737a300517ce2e42ab25f1d00ca72e2e0007
redgradient <neutral1shade@yahoo.com>
2023-02-24 20:54:38 +0400

    added class annotations

--- app/src/test/java/hexlet/code/AppApplicationTests.java
+++ app/src/test/java/hexlet/code/AppApplicationTests.java
@@ -1,27 +1,32 @@
 package hexlet.code;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import hexlet.code.config.SpringConfigForIT;
 import hexlet.code.models.User;
 import hexlet.code.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static hexlet.code.config.SpringConfigForIT.TEST_PROFILE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
 
 @AutoConfigureMockMvc
-//@ActiveProfiles(TEST_PROFILE)
-//@SpringBootTest(webEnvironment = RANDOM_PORT, classes = SpringConfigForIT.class)
+@ActiveProfiles(TEST_PROFILE)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = SpringConfigForIT.class)
+@ExtendWith(SpringExtension.class)
 //@DataSet("datasets/users.yml")
 class AppApplicationTests {
 
